3721e658a9fcf8f37d7076df997a607467c965d4
KapNemo31 <93931054+KapNemo31@users.noreply.github.com>
2021-12-26 21:45:28 +0300

    Add files via upload

---
+++ mysite/index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Визитка';
+$name = 'Темный фрилансер';
+$image = 'img/SKILLET.svg';
+$email = 'darkkingnemo@gmail.com';
+$phone = '+79516764049';
+
+require('components/header.php');
+require('components/about.php');
+require('components/status.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
