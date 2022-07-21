d90c166cbeeb6f8be1c40623a87d92427a7575b9
Yura <qwerasdfzxcvqwerasdfzxcv635@gmail.com>
2022-07-21 22:48:43 +0300

    2 новых файла

---
+++ greatmovie/script.js
@@ -0,0 +1,6 @@
+let button = document.querySelector('.burger-menu_button');
+let menu = document.querySelector('.burger-menu');
+
+function toggleMenu() {
+    menu.classList.toggle('burger-menu_active');
+}
